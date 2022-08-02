2908baf2f9afe4f3f5846123c774e208f3144ff2
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-08-02 17:05:53 -0400

    end of day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeroesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
