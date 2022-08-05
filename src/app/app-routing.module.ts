142194d33fb4b66d150990a7a65505c3bc1d5e74
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-08-05 11:35:55 -0400

    visibility to schematics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroAddressFormComponent } from './hero-address-form/hero-address-form.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { NewDashComponent } from './new-dash/new-dash.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'shipping-address-form', component: HeroAddressFormComponent },
+  { path: 'fun-dashboard-example', component: NewDashComponent },
 ];
 
 @NgModule({
