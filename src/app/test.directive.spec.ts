ef2fec62ceac1f5e53a5d04ed88c4d3b81bece6a
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-08-10 12:05:47 -0400

    fix tests

--- src/app/test.directive.spec.ts
+++ src/app/test.directive.spec.ts
@@ -1,8 +1,35 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 import { TestDirective } from './test.directive';
 
 describe('TestDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TestDirective();
-    expect(directive).toBeTruthy();
+  @Component({
+    template: ` <h2 appTest="yellow">Something Yellow</h2>
+      <h2 appTest>The Default (Gray)</h2>
+      <h2>No Highlight</h2>
+      <input #box [appTest]="box.value" value="cyan" />`,
+  })
+  class TestComponent {}
+
+  let fixture: ComponentFixture<TestComponent>;
+  let des;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [TestDirective, TestComponent],
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges(); // initial binding
+
+    // all elements with an attached TestDirective
+    des = fixture.debugElement.queryAll(By.directive(TestDirective));
+
+    // the h2 without the TestDirective
+    fixture.debugElement.query(By.css('h2:not([appTest])'));
+  });
+
+  it('should work', () => {
+    expect(des.length).toBe(3);
   });
 });
