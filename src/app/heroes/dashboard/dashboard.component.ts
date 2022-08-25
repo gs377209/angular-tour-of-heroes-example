980bf02452a9232db252a1549a38edc6d82539be
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-08-25 12:07:39 -0400

    start of i18n

--- src/app/heroes/dashboard/dashboard.component.ts
+++ src/app/heroes/dashboard/dashboard.component.ts
@@ -10,11 +10,20 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  today = new Date();
+  minutes = 2;
+  amount = 9999.99;
+  logo = 'assets/favicon.ico';
+  translated = $localize`:basic text|A sample translation:Hello!`;
+  translatedWithVar = '';
+  gender = 'n/a';
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
+    const person = 'Gerrod';
+    this.translatedWithVar = $localize`Hello ${person}!`;
   }
 
   getHeroes(): void {
